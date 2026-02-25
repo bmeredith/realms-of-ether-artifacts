07ce4a6e836ccce976f0cc5c9de7bb4d1c90f830
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-02-25 15:53:20 -0600

    added returns for getGold/Wood/Stone

--- contracts/reconstructed/BuildingStorage.sol
+++ contracts/reconstructed/BuildingStorage.sol
@@ -33,12 +33,16 @@ contract BuildingStorage {
         revert();
     }
 
-    function 0x08f822a5(uint256 varg0) public { 
+    function 0x08f822a5(uint256 varg0)
+     public 
+    { 
         require(_getName[varg0]);
         return mapping_7[varg0];
     }
 
-    function 0x21d26a38(uint256 varg0) public { 
+    function 0x21d26a38(uint256 varg0) 
+        public 
+    { 
         require(msg.sender == owner);
         require(bool(!_getName[varg0]));
         _getIndexLength.length += 1;
@@ -53,101 +57,141 @@ contract BuildingStorage {
         _getName[varg0] = 1;
     }
 
-    function 0x2c5e9b09(uint256 varg0, uint256 varg1) public { 
+    function 0x2c5e9b09(uint256 varg0, uint256 varg1) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[varg0]);
         mapping_6[varg0] = varg1;
     }
 
-    function 0x394c1f42(uint256 varg0, uint256 varg1) public { 
+    function 0x394c1f42(uint256 varg0, uint256 varg1) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[varg0]);
         mapping_5[varg0] = varg1;
     }
 
-    function 0x4610af12(uint256 varg0, uint256 varg1) public { 
+    function 0x4610af12(uint256 varg0, uint256 varg1) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[varg0]);
         mapping_4[varg0] = varg1;
     }
 
-    function 0x50fdbd81(uint256 varg0) public { 
+    function 0x50fdbd81(uint256 varg0) 
+        public 
+    { 
         require(_getName[varg0]);
         return mapping_5[varg0];
     }
 
-    function getName(bytes32 saleId) public { 
+    function getName(bytes32 saleId) 
+        public 
+    { 
         require(_getName[uint256(saleId)]);
         return bytes16(mapping_3[uint256(saleId)] << 128);
     }
 
-    function getHash(uint256 _nonce) public { 
+    function getHash(uint256 _nonce) 
+        public 
+    { 
         assert(_nonce < _getIndexLength.length);
         return uint256(_getIndexLength[_nonce]);
     }
 
-    function setStone(bytes32 _buildingHash, uint256 _amount) public { 
+    function setStone(bytes32 _buildingHash, uint256 _amount) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[_buildingHash]);
         stone[_buildingHash] = _amount;
     }
 
-    function setGold(bytes32 _buildingHash, uint256 _amount) public { 
+    function setGold(bytes32 _buildingHash, uint256 _amount) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[_buildingHash]);
         gold[_buildingHash] = _amount;
     }
 
-    function 0x8d4fd8c1(uint256 varg0, uint256 varg1) public { 
+    function 0x8d4fd8c1(uint256 varg0, uint256 varg1) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[varg0]);
         mapping_7[varg0] = varg1;
     }
 
-    function setWood(bytes32 _buildingHash, uint256 _amount) public { 
+    function setWood(bytes32 _buildingHash, uint256 _amount) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[_buildingHash]);
         wood[_buildingHash] = _amount;
     }
 
-    function 0xdce9f070(uint256 varg0) public { 
+    function 0xdce9f070(uint256 varg0) 
+        public 
+    { 
         require(_getName[varg0]);
         return mapping_4[varg0];
     }
 
-    function getWood(bytes32 _buildingHash) public { 
+    function getWood(bytes32 _buildingHash) 
+        public 
+        returns (uint256)
+    { 
         require(_getName[_buildingHash]);
         return wood[_buildingHash];
     }
 
-    function getIndexLength() public { 
+    function getIndexLength() 
+        public 
+        returns (uint256)
+    { 
         return _getIndexLength.length;
     }
 
-    function getStone(bytes32 _buildingHash) public { 
+    function getStone(bytes32 _buildingHash) 
+        public 
+        returns (uint256)
+    { 
         require(_getName[_buildingHash]);
         return stone[_buildingHash];
     }
 
-    function getGold(bytes32 _buildingHash) public { 
+    function getGold(bytes32 _buildingHash) 
+        public
+        returns (uint256)
+    { 
         require(_getName[_buildingHash]);
         return gold[_buildingHash];
     }
 
-    function transferOwnership(address newOwner) public { 
+    function transferOwnership(address newOwner) 
+        public 
+    { 
         require(msg.sender == owner);
         require(bool(newOwner != address(0x0)));
-        emit OwnershipTransferred(owner, newOwner);
+        OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 
-    function 0xf776c071(uint256 varg0, bytes16 varg1) public { 
+    function 0xf776c071(uint256 varg0, bytes16 varg1) 
+        public 
+    { 
         require(msg.sender == owner);
         require(_getName[varg0]);
         mapping_3[varg0] = varg1 >> 128 | bytes16(mapping_3[varg0]);
     }
 
-    function 0xfe6f1143(uint256 varg0) public { 
+    function 0xfe6f1143(uint256 varg0) 
+        public 
+    { 
         require(_getName[varg0]);
         return uint256(mapping_6[varg0]);
     }
