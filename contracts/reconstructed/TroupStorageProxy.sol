34bce0f0b604ed8763844d4302919082416301d6
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-02-25 20:21:32 -0600

    fixed up whitespace

--- contracts/reconstructed/TroupStorageProxy.sol
+++ contracts/reconstructed/TroupStorageProxy.sol
@@ -2,98 +2,98 @@ pragma solidity 0.4.18;
 
 contract TroupStorageProxy {
     function 0x93fdc929(address varg0, uint256 varg1, bytes16 varg2, uint256 varg3, uint256 varg4, uint256 varg5, uint256 varg6, uint256 varg7, uint256 varg8, uint256 varg9) public payable { 
-      require(bool(varg0.code.size));
-      v0 = varg0.call(uint32(0x7a65efc9), varg1).gas(msg.gas - 710);
-      require(bool(v0));
-      require(bool(varg0.code.size));
-      v1 = varg0.call(uint32(0xf776c071), varg1, varg2).gas(msg.gas - 710);
-      require(bool(v1));
-      require(bool(varg0.code.size));
-      v2 = varg0.call(uint32(0xc74d8903), varg1, varg3).gas(msg.gas - 710);
-      require(bool(v2));
-      require(bool(varg0.code.size));
-      v3 = varg0.call(uint32(0x80461f9), varg1, varg4).gas(msg.gas - 710);
-      require(bool(v3));
-      require(bool(varg0.code.size));
-      v4 = varg0.call(uint32(0x41731f8b), varg1, varg5).gas(msg.gas - 710);
-      require(bool(v4));
-      require(bool(varg0.code.size));
-      v5 = varg0.call(uint32(0x718de536), varg1, varg6).gas(msg.gas - 710);
-      require(bool(v5));
-      require(bool(varg0.code.size));
-      v6 = varg0.call(uint32(0x70c92125), varg1, varg7).gas(msg.gas - 710);
-      require(bool(v6));
-      require(bool(varg0.code.size));
-      v7 = varg0.call(uint32(0x9c8b8588), varg1, varg8).gas(msg.gas - 710);
-      require(bool(v7));
-      require(bool(varg0.code.size));
-      v8 = varg0.call(uint32(0x6d0af38e), varg1, varg9).gas(msg.gas - 710);
-      require(bool(v8));
-  }
+        require(bool(varg0.code.size));
+        v0 = varg0.call(uint32(0x7a65efc9), varg1).gas(msg.gas - 710);
+        require(bool(v0));
+        require(bool(varg0.code.size));
+        v1 = varg0.call(uint32(0xf776c071), varg1, varg2).gas(msg.gas - 710);
+        require(bool(v1));
+        require(bool(varg0.code.size));
+        v2 = varg0.call(uint32(0xc74d8903), varg1, varg3).gas(msg.gas - 710);
+        require(bool(v2));
+        require(bool(varg0.code.size));
+        v3 = varg0.call(uint32(0x80461f9), varg1, varg4).gas(msg.gas - 710);
+        require(bool(v3));
+        require(bool(varg0.code.size));
+        v4 = varg0.call(uint32(0x41731f8b), varg1, varg5).gas(msg.gas - 710);
+        require(bool(v4));
+        require(bool(varg0.code.size));
+        v5 = varg0.call(uint32(0x718de536), varg1, varg6).gas(msg.gas - 710);
+        require(bool(v5));
+        require(bool(varg0.code.size));
+        v6 = varg0.call(uint32(0x70c92125), varg1, varg7).gas(msg.gas - 710);
+        require(bool(v6));
+        require(bool(varg0.code.size));
+        v7 = varg0.call(uint32(0x9c8b8588), varg1, varg8).gas(msg.gas - 710);
+        require(bool(v7));
+        require(bool(varg0.code.size));
+        v8 = varg0.call(uint32(0x6d0af38e), varg1, varg9).gas(msg.gas - 710);
+        require(bool(v8));
+    }
 
-  function upgrade(address proxy, address implementation) public payable { 
-      require(bool(proxy.code.size));
-      v0 = proxy.transferOwnership(implementation).gas(msg.gas - 710);
-      require(bool(v0));
-  }
+    function upgrade(address proxy, address implementation) public payable { 
+        require(bool(proxy.code.size));
+        v0 = proxy.transferOwnership(implementation).gas(msg.gas - 710);
+        require(bool(v0));
+    }
 
-  function getHash(address _tokenAddress, uint256 _tokenId) public payable { 
-      MEM[32 + MEM[64]] = 0;
-      require(bool(_tokenAddress.code.size));
-      v0, /* uint256 */ v1 = _tokenAddress.getHash(_tokenId).gas(msg.gas - 710);
-      require(bool(v0));
-      return uint256(v1);
-  }
+    function getHash(address _tokenAddress, uint256 _tokenId) public payable { 
+        MEM[32 + MEM[64]] = 0;
+        require(bool(_tokenAddress.code.size));
+        v0, /* uint256 */ v1 = _tokenAddress.getHash(_tokenId).gas(msg.gas - 710);
+        require(bool(v0));
+        return uint256(v1);
+    }
 
-  function 0xf5807181(address varg0) public payable { 
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v0, /* uint256 */ v1 = varg0.getIndexLength().gas(msg.gas - 710);
-      require(bool(v0));
-      return v1;
-  }
+    function 0xf5807181(address varg0) public payable { 
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v0, /* uint256 */ v1 = varg0.getIndexLength().gas(msg.gas - 710);
+        require(bool(v0));
+        return v1;
+    }
 
-  function 0xfc85a0d2(address varg0, uint256 varg1) public payable { 
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v0, /* uint256 */ v1 = varg0.call(uint32(0xe75f7871), varg1).gas(msg.gas - 710);
-      require(bool(v0));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v2, /* uint256 */ v3 = varg0.call(uint32(0xe382af35), varg1).gas(msg.gas - 710);
-      require(bool(v2));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v4, /* uint256 */ v5 = varg0.call(uint32(0xe0d87dc2), varg1).gas(msg.gas - 710);
-      require(bool(v4));
-      return v1, v3, v5;
-  }
+    function 0xfc85a0d2(address varg0, uint256 varg1) public payable { 
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v0, /* uint256 */ v1 = varg0.call(uint32(0xe75f7871), varg1).gas(msg.gas - 710);
+        require(bool(v0));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v2, /* uint256 */ v3 = varg0.call(uint32(0xe382af35), varg1).gas(msg.gas - 710);
+        require(bool(v2));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v4, /* uint256 */ v5 = varg0.call(uint32(0xe0d87dc2), varg1).gas(msg.gas - 710);
+        require(bool(v4));
+        return v1, v3, v5;
+    }
 
-  function fallback() public payable { 
-      revert();
-  }
+    function fallback() public payable { 
+        revert();
+    }
 
-  function 0x2a33569e(address varg0, uint256 varg1) public payable { 
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v0, /* bytes16 */ v1 = varg0.getName(varg1).gas(msg.gas - 710);
-      require(bool(v0));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v2, /* uint256 */ v3 = varg0.call(uint32(0x82f0b31c), varg1).gas(msg.gas - 710);
-      require(bool(v2));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v4, /* uint256 */ v5 = varg0.call(uint32(0x6453da9a), varg1).gas(msg.gas - 710);
-      require(bool(v4));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v6, /* uint256 */ v7 = varg0.call(uint32(0x79525281), varg1).gas(msg.gas - 710);
-      require(bool(v6));
-      MEM[32 + MEM[64]] = 0;
-      require(bool(varg0.code.size));
-      v8, /* uint256 */ v9 = varg0.call(uint32(0x9342ccc2), varg1).gas(msg.gas - 710);
-      require(bool(v8));
-      return bytes16(v1), v3, v5, v7, v9;
-  }
+    function 0x2a33569e(address varg0, uint256 varg1) public payable { 
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v0, /* bytes16 */ v1 = varg0.getName(varg1).gas(msg.gas - 710);
+        require(bool(v0));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v2, /* uint256 */ v3 = varg0.call(uint32(0x82f0b31c), varg1).gas(msg.gas - 710);
+        require(bool(v2));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v4, /* uint256 */ v5 = varg0.call(uint32(0x6453da9a), varg1).gas(msg.gas - 710);
+        require(bool(v4));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v6, /* uint256 */ v7 = varg0.call(uint32(0x79525281), varg1).gas(msg.gas - 710);
+        require(bool(v6));
+        MEM[32 + MEM[64]] = 0;
+        require(bool(varg0.code.size));
+        v8, /* uint256 */ v9 = varg0.call(uint32(0x9342ccc2), varg1).gas(msg.gas - 710);
+        require(bool(v8));
+        return bytes16(v1), v3, v5, v7, v9;
+    }
 }
