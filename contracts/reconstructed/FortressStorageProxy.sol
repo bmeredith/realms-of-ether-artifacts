881fb4fa958f41b34176278e9a3f4a94a3173ebd
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-02-26 00:44:30 -0600

    implemented functions with IFortressStorage

--- contracts/reconstructed/FortressStorageProxy.sol
+++ contracts/reconstructed/FortressStorageProxy.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.4.18;
 
+import {IFortressStorage} from "./interfaces/IFortressStorage.sol";
+
 /// @title FortressStorageProxy for Realms of Ether (https://www.realmsofether.com)
 /// @notice Reconstructed by wilt.eth/@wilty_stilty
 ///
@@ -11,7 +13,6 @@ pragma solidity 0.4.18;
 ///
 /// @dev RECONSTRUCTION NOTICE: The original source code for this contract was lost.
 /// This file has been reconstructed in its entirety from the deployed bytecode.
-
 contract FortressStorageProxy {
     function getIndexLength(
         address _fortressStorage, 
@@ -20,12 +21,8 @@ contract FortressStorageProxy {
         public
         payable 
         returns (uint256)
-    { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = varg0.call(uint32(0xf5807181), _user).gas(msg.gas - 710);
-        require(bool(v0));
-        return v1;
+    {
+        return IFortressStorage(_fortressStorage).getIndexLength(_user);
     }
 
     function getFortress(
@@ -42,33 +39,13 @@ contract FortressStorageProxy {
             uint256
         )
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* bytes16 */ v1 = _fortressStorage.getName(_fortressHash).gas(msg.gas - 710);
-        require(bool(v0));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v2, /* address */ v3 = _fortressStorage.getOwner(_fortressHash).gas(msg.gas - 710);
-        require(bool(v2));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xcdf28eaf = getX(bytes32)
-        v4, /* uint256 */ v5 = _fortressStorage.call(uint32(0xcdf28eaf), _fortressHash).gas(msg.gas - 710);
-        require(bool(v4));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0x3141d0db = getY(bytes32)
-        v6, /* uint256 */ v7 = _fortressStorage.call(uint32(0x3141d0db), _fortressHash).gas(msg.gas - 710);
-        require(bool(v6));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xb6d7e8a1 = getWins(bytes32)
-        v8, /* uint256 */ v9 = _fortressStorage.call(uint32(0xb6d7e8a1), _fortressHash).gas(msg.gas - 710);
-        require(bool(v8));
-        return bytes16(MEM[MEM[64]]), address(v3), v5, v7, v9;
+        return (
+            IFortressStorage(_fortressStorage).getName(_fortressHash),
+            IFortressStorage(_fortressStorage).getOwner(_fortressHash),
+            IFortressStorage(_fortressStorage).getX(_fortressHash),
+            IFortressStorage(_fortressStorage).getY(_fortressHash),
+            IFortressStorage(_fortressStorage).getWins(_fortressHash)
+        );
     }
 
     function setOwner(
@@ -79,9 +56,7 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = varg0.setOwner(_fortressHash, _newOwner).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage).setOwner(_fortressHash, _newOwner);
     }
 
     function setGold(
@@ -92,9 +67,7 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.call(uint32(0x70c92125), _fortressHash, _amount).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage).setGold(_fortressHash, _amount);
     }
 
     function getFortressesAvailable(address _fortressStorage)
@@ -102,11 +75,7 @@ contract FortressStorageProxy {
         payable 
         returns (uint256)
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0x468c7804)).gas(msg.gas - 710);
-        require(bool(v0));
-        return v1;
+        return IFortressStorage(_fortressStorage).getFortressesAvailable();
     }
 
     function getFortressCount(address _fortressStorage) 
@@ -114,11 +83,7 @@ contract FortressStorageProxy {
         payable 
         returns (uint256)
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0x10cfcf0c)).gas(msg.gas - 710);
-        require(bool(v0));
-        return v1;
+        return IFortressStorage(_fortressStorage).getFortressCount();
     }
 
     function getBuilding(
@@ -133,19 +98,12 @@ contract FortressStorageProxy {
             uint256
         )
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0x31857c44 = getBuildingLevel(bytes32,bytes32)
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0x31857c44), _fortressHash, _buildingHash).gas(msg.gas - 710);
-        require(bool(v0));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xb41cddba = getBuildingTimeout(bytes32,bytes32)
-        v2, /* uint256 */ v3 = _fortressStorage.call(uint32(0xb41cddba), _fortressHash, _buildingHash).gas(msg.gas - 710);
-        require(bool(v2));
-        return v1, v3;
+        return (
+            IFortressStorage(_fortressStorage)
+                .getBuildingLevel(_fortressHash, _buildingHash),
+            IFortressStorage(_fortressStorage)
+                .getBuildingTimeout(_fortressHash, _buildingHash)
+        );
     }
 
     function getHashFromIndex(
@@ -157,11 +115,8 @@ contract FortressStorageProxy {
         payable 
         returns (bytes32)
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0x78a11bf0), _user, _index).gas(msg.gas - 710);
-        require(bool(v0));
-        return uint256(v1);
+        return IFortressStorage(_fortressStorage)
+            .getHashFromIndex(_user, _index);
     }
 
     function setTroups(
@@ -173,9 +128,8 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.call(uint32(0x45c7d3a6), _fortressHash, _troupHash, _amount).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage)
+            .setTroups(_fortressHash, _troupHash, _amount);
     }
 
     function getResources(
@@ -190,25 +144,11 @@ contract FortressStorageProxy {
             uint256
         )
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xe75f7871 = getGold(bytes32)
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0xe75f7871), _fortressHash).gas(msg.gas - 710);
-        require(bool(v0));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xe382af35 = getStone(bytes32)
-        v2, /* uint256 */ v3 = _fortressStorage.call(uint32(0xe382af35), _fortressHash).gas(msg.gas - 710);
-        require(bool(v2));
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0xe0d87dc2 = getWood(bytes32)
-        v4, /* uint256 */ v5 = _fortressStorage.call(uint32(0xe0d87dc2), _fortressHash).gas(msg.gas - 710);
-        require(bool(v4));
-        return v1, v3, v5;
+        return (
+            IFortressStorage(_fortressStorage).getGold(_fortressHash),
+            IFortressStorage(_fortressStorage).getStone(_fortressHash),
+            IFortressStorage(_fortressStorage).getWood(_fortressHash)
+        );
     }
 
     function getOwner(
@@ -219,11 +159,7 @@ contract FortressStorageProxy {
         payable 
         returns (address)
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(account.code.size));
-        v0, /* address */ v1 = _fortressStorage.getOwner(uint256(_fortressHash)).gas(msg.gas - 710);
-        require(bool(v0));
-        return address(v1);
+        return IFortressStorage(_fortressStorage).getOwner(_fortressHash);
     }
 
     function build(
@@ -238,26 +174,11 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-
-        // 0x70c92125 = setGold(bytes32,uint256)
-        v0 = varg0.call(uint32(0x70c92125), _fortressHash, _gold).gas(msg.gas - 710);
-        require(bool(v0));
-        require(bool(varg0.code.size));
-
-        // 0x6d0af38e = setStone(bytes32,uint256)
-        v1 = varg0.call(uint32(0x6d0af38e), _fortressHash, _stone).gas(msg.gas - 710);
-        require(bool(v1));
-        require(bool(varg0.code.size));
-
-        // 0x9c8b8588 = setWood(bytes32,uint256)
-        v2 = varg0.call(uint32(0x9c8b8588), _fortressHash, _wood).gas(msg.gas - 710);
-        require(bool(v2));
-        require(bool(varg0.code.size));
-
-        // 0x8ff4efb7 = setBuildingLevel(bytes32,bytes32,uint256)
-        v3 = varg0.call(uint32(0x8ff4efb7), _fortressHash, _buildingHash, _level).gas(msg.gas - 710);
-        require(bool(v3));
+        IFortressStorage(_fortressStorage).setGold(_fortressHash, _gold);
+        IFortressStorage(_fortressStorage).setStone(_fortressHash, _stone);
+        IFortressStorage(_fortressStorage).setWood(_fortressHash, _wood);
+        IFortressStorage(_fortressStorage)
+            .setBuildingLevel(_fortressHash, _buildingHash, _level);
     }
 
     function setStone(
@@ -268,18 +189,14 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.call(uint32(0x6d0af38e), _fortressHash, _amount).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage).setStone(_fortressHash, _amount);
     }
 
     function upgrade(address proxy, address implementation)
         public 
         payable 
     { 
-        require(bool(proxy.code.size));
-        v0 = proxy.transferOwnership(implementation).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(proxy).transferOwnership(implementation);
     }
 
     function setWood(
@@ -290,9 +207,7 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.call(uint32(0x9c8b8588), _fortressHash, _amount).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage).setWood(_fortressHash, _amount);
     }
 
     function transfer(
@@ -303,9 +218,7 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.setOwner(_fortressHash, _newOwner).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage).setOwner(_fortressHash, _newOwner);
     }
 
     function createFortress(
@@ -317,58 +230,27 @@ contract FortressStorageProxy {
         uint256 _gold, 
         uint256 _stone, 
         uint256 _wood, 
-        uint256 _level, 
+        uint256 _wins, 
         address _user
     ) 
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-
-        // 0x43244b61 = createFortress(bytes32,address)
-        v0 = _fortressStorage.call(uint32(0x43244b61), _fortressHash, _user).gas(msg.gas - 710);
-        require(bool(v0));
-        require(bool(varg0.code.size));
-
-        // 0xf776c071 = setName(bytes32,bytes16)
-        v1 = _fortressStorage.call(uint32(0xf776c071), _fortressHash, _name).gas(msg.gas - 710);
-        require(bool(v1));
-        require(bool(varg0.code.size));
-
-        // 0x69c6821d = setX(bytes32,int256)
-        v2 = _fortressStorage.call(uint32(0x69c6821d), _fortressHash, _x).gas(msg.gas - 710);
-        require(bool(v2));
-        require(bool(varg0.code.size));
-
-        // 0xb874b685 = setY(bytes32,int256)
-        v3 = _fortressStorage.call(uint32(0xb874b685), _fortressHash, _y).gas(msg.gas - 710);
-        require(bool(v3));
-        require(bool(varg0.code.size));
-
-        // 0x70c92125 = setGold(bytes32,uint256)
-        v4 = _fortressStorage.call(uint32(0x70c92125), _fortressHash, _gold).gas(msg.gas - 710);
-        require(bool(v4));
-        require(bool(varg0.code.size));
-
-        // 0x6d0af38e = setStone(bytes32,uint256)
-        v5 = _fortressStorage.call(uint32(0x6d0af38e), _fortressHash, _stone).gas(msg.gas - 710);
-        require(bool(v5));
-        require(bool(varg0.code.size));
-
-        // 0x9c8b8588 = setWood(bytes32,uint256)
-        v6 = _fortressStorage.call(uint32(0x9c8b8588), _fortressHash, _wood).gas(msg.gas - 710);
-        require(bool(v6));
-        require(bool(varg0.code.size));
-
-        // 0x2fb59b80 = setLevel(bytes32,uint256)
-        v7 = _fortressStorage.call(uint32(0x2fb59b80), _fortressHash, _level).gas(msg.gas - 710);
-        require(bool(v7));
+        IFortressStorage(_fortressStorage).createFortress(_fortressHash, _user);
+        IFortressStorage(_fortressStorage).setName(_fortressHash, _name);
+        IFortressStorage(_fortressStorage).setX(_fortressHash, _x);
+        IFortressStorage(_fortressStorage).setY(_fortressHash, _y);
+        IFortressStorage(_fortressStorage).setGold(_fortressHash, _gold);
+        IFortressStorage(_fortressStorage).setStone(_fortressHash, _stone);
+        IFortressStorage(_fortressStorage).setWood(_fortressHash, _wood);
+        IFortressStorage(_fortressStorage).setWins(_fortressHash, _wins);
     }
 
-    function startMinting(address _fortressStorage) public payable { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.startMinting().gas(msg.gas - 710);
-        require(bool(v0));
+    function startMinting(address _fortressStorage) 
+        public 
+        payable 
+    { 
+        IFortressStorage(_fortressStorage).startMinting();
     }
 
     function setBuildingTimeout(
@@ -380,9 +262,8 @@ contract FortressStorageProxy {
         public 
         payable 
     { 
-        require(bool(varg0.code.size));
-        v0 = _fortressStorage.call(uint32(0xb9d8a1cf), _fortressHash, _buildingHash, _timeout).gas(msg.gas - 710);
-        require(bool(v0));
+        IFortressStorage(_fortressStorage)
+            .setBuildingTimeout(_fortressHash, _buildingHash, _timeout);
     }
 
     function getTroups(
@@ -394,13 +275,7 @@ contract FortressStorageProxy {
         payable 
         returns (uint256)
     { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-
-        // 0x6c4426be = getTroups(bytes32,bytes32)
-        v0, /* uint256 */ v1 = _fortressStorage.call(uint32(0x6c4426be), _fortressHash, _troupHash).gas(msg.gas - 710);
-        require(bool(v0));
-        return v1;
+        IFortressStorage(_fortressStorage).getTroups(_fortressHash, _troupHash);
     }
 
     function() public payable {
