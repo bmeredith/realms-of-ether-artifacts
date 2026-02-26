1032ad32e9ac42dee0e28ea04a7e37dc12096c83
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-02-25 20:39:23 -0600

    identified all function sigs and their parameters

--- contracts/reconstructed/TroupStorageProxy.sol
+++ contracts/reconstructed/TroupStorageProxy.sol
@@ -44,40 +44,47 @@ contract TroupStorageProxy {
         require(bool(v8));
     }
 
-    function upgrade(address proxy, address implementation) public payable { 
+    function upgrade(address proxy, address implementation) 
+        public 
+    { 
         require(bool(proxy.code.size));
         v0 = proxy.transferOwnership(implementation).gas(msg.gas - 710);
         require(bool(v0));
     }
 
-    function getHash(address _tokenAddress, uint256 _tokenId) public payable { 
-        MEM[32 + MEM[64]] = 0;
-        require(bool(_tokenAddress.code.size));
-        v0, /* uint256 */ v1 = _tokenAddress.getHash(_tokenId).gas(msg.gas - 710);
+    function getHash(address _troupStorage, uint256 _nonce) 
+        public 
+    {
+        require(bool(_troupStorage.code.size));
+        v0, /* uint256 */ v1 = _troupStorage.getHash(_nonce).gas(msg.gas - 710);
         require(bool(v0));
         return uint256(v1);
     }
 
-    function 0xf5807181(address varg0) public payable { 
+    function getIndexLength(address _troupStorage) 
+        public 
+    { 
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = varg0.getIndexLength().gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v0, /* uint256 */ v1 = _troupStorage.getIndexLength().gas(msg.gas - 710);
         require(bool(v0));
         return v1;
     }
 
-    function 0xfc85a0d2(address varg0, uint256 varg1) public payable { 
+    function getCosts(address _troupStorage, bytes32 _troupHash) 
+        public 
+    { 
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* uint256 */ v1 = varg0.call(uint32(0xe75f7871), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v0, /* uint256 */ v1 = _troupStorage.call(uint32(0xe75f7871), _troupHash).gas(msg.gas - 710);
         require(bool(v0));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v2, /* uint256 */ v3 = varg0.call(uint32(0xe382af35), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v2, /* uint256 */ v3 = _troupStorage.call(uint32(0xe382af35), _troupHash).gas(msg.gas - 710);
         require(bool(v2));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v4, /* uint256 */ v5 = varg0.call(uint32(0xe0d87dc2), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v4, /* uint256 */ v5 = _troupStorage.call(uint32(0xe0d87dc2), _troupHash).gas(msg.gas - 710);
         require(bool(v4));
         return v1, v3, v5;
     }
@@ -86,26 +93,28 @@ contract TroupStorageProxy {
         revert();
     }
 
-    function 0x2a33569e(address varg0, uint256 varg1) public payable { 
+    function getTroup(address _troupStorage, bytes32 _troupHash)
+        public 
+    { 
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v0, /* bytes16 */ v1 = varg0.getName(varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v0, /* bytes16 */ v1 = _troupStorage.getName(_troupHash).gas(msg.gas - 710);
         require(bool(v0));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v2, /* uint256 */ v3 = varg0.call(uint32(0x82f0b31c), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v2, /* uint256 */ v3 = _troupStorage.call(uint32(0x82f0b31c), _troupHash).gas(msg.gas - 710);
         require(bool(v2));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v4, /* uint256 */ v5 = varg0.call(uint32(0x6453da9a), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v4, /* uint256 */ v5 = _troupStorage.call(uint32(0x6453da9a), _troupHash).gas(msg.gas - 710);
         require(bool(v4));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v6, /* uint256 */ v7 = varg0.call(uint32(0x79525281), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v6, /* uint256 */ v7 = _troupStorage.call(uint32(0x79525281), _troupHash).gas(msg.gas - 710);
         require(bool(v6));
         MEM[32 + MEM[64]] = 0;
-        require(bool(varg0.code.size));
-        v8, /* uint256 */ v9 = varg0.call(uint32(0x9342ccc2), varg1).gas(msg.gas - 710);
+        require(bool(_troupStorage.code.size));
+        v8, /* uint256 */ v9 = _troupStorage.call(uint32(0x9342ccc2), _troupHash).gas(msg.gas - 710);
         require(bool(v8));
         return bytes16(v1), v3, v5, v7, v9;
     }
